5b3f758d6c01f111f533a6aa87168da822860eba
Andrew Marcuse <fileformat@gmail.com>
2026-06-28 20:24:51 -0400

    Fix for status.json

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# build Astro site
+#
 
 set -o errexit
 set -o pipefail
@@ -6,10 +9,20 @@ set -o nounset
 
 echo "INFO: build starting at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
 
-echo "INFO: Node version = $(node --version)"
+echo "INFO: Node location = $(which node)"
 echo "INFO: npm version = $(npm --version)"
 
-./generate_base64.sh
+echo "INFO: CI=${CI:-not set}"
+echo "INFO: CF_PAGES=${CF_PAGES:-not set}"
+echo "INFO: WORKERS_CI=${WORKERS_CI:-not set}"
+echo "INFO: WORKERS_CI_COMMIT_SHA=${WORKERS_CI_COMMIT_SHA:-not set}"
+
+echo "INFO: setting build information"
+jq -n \
+    --arg commit "${WORKERS_CI_COMMIT_SHA:0:7}" \
+    --arg lastmod "$(date -u +%Y-%m-%dT%H:%M:%SZ)" \
+    '{commit: $commit, lastmod: $lastmod}' \
+    > src/data/build.json
 
 npm run build
 
--- src/pages/status.json.ts
+++ src/pages/status.json.ts
@@ -1,48 +1,13 @@
 import type { APIRoute } from "astro";
-import { execSync } from "node:child_process";
-
-const COMMIT_ENV_VARS = [
-    "COMMIT_REF", // Netlify
-    "GITHUB_SHA", // Github Actions
-    "WORKERS_CI_COMMIT_SHA", // Cloudflare Workers CI
-    "CF_PAGES_COMMIT_SHA", // Cloudflare Pages
-    "CI_COMMIT_SHA", // Gitlab CI
-];
-
-const getCommit = () => {
-    for (const envVar of COMMIT_ENV_VARS) {
-        const commit = process.env[envVar];
-        if (commit) {
-            return commit.slice(0, 7);
-        }
-    }
-
-    let retVal = "null";
-    try {
-        retVal = execSync("git rev-parse --short HEAD").toString().trim();
-    } catch (err) {
-        return `error-${err}`;
-    }
-
-    try {
-        const isDirty = execSync('git diff --quiet || echo "dirty"')
-            .toString()
-            .trim();
-        if (isDirty === "dirty") {
-            retVal = `${retVal}-dirty`;
-        }
-    } catch {
-        return `${retVal}-error`;
-    }
-    return retVal;
-};
+import buildInfo from "../data/build.json";
 
 export const GET: APIRoute = (context) => {
     const payload = {
         success: true,
         message: "OK",
-        commit: getCommit(),
-        lastmod: new Date().toISOString(),
+        commit: buildInfo.commit || "",
+        lastmod: buildInfo.lastmod || "",
+        timestamp: new Date().toISOString(),
         tech: context.generator,
     };
 
@@ -56,5 +21,3 @@ export const GET: APIRoute = (context) => {
         },
     });
 };
-
-export const prerender = true;
